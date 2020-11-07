da776553f6a08471fb18d2110bae7f984acb3bfb
Elenapark <60565155+Elenapark@users.noreply.github.com>
2020-11-07 16:01:11 +0900

    JavaScript: Navbar menu click

--- main.js
+++ main.js
@@ -4,9 +4,24 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
       if(window.scrollY > navbarHeight){
         navbar.classList.add("navbar--dark");
     }else {
         navbar.classList.remove("navbar--dark");
     }
 })
+// navbar 메뉴 클릭 시 스크롤 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+    const target = e.target;
+    const link = target.dataset.link;
+    console.log(link);
+    if(link === null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    console.log(scrollTo);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
+
